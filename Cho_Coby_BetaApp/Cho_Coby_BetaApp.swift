1c7584ca5e53ccb5d6cac480c60572095e8391a5
CobyCho <cobycho7@gmail.com>
2024-10-24 11:56:08 -0500

    Add swiftUI files

---
+++ Cho_Coby_BetaApp/Cho_Coby_BetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cho_Coby_BetaAppApp.swift
+//  Cho_Coby_BetaApp
+//
+//  Created by user264337 on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Cho_Coby_BetaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
