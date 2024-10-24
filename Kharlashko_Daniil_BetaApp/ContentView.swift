e18c6fedb6941d74e96caa47af37ae945df7cb73
Dankharl <Dankharl12345678@gmail.com>
2024-10-24 01:55:15 -0500

    Initial Commit

---
+++ Kharlashko_Daniil_BetaApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kharlashko_Daniil_BetaApp
+//
+//  Created by Daniil Kharlashko on 24/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kharlashko_Daniil_BetaApp/Kharlashko_Daniil_BetaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kharlashko_Daniil_BetaAppApp.swift
+//  Kharlashko_Daniil_BetaApp
+//
+//  Created by Daniil Kharlashko on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Kharlashko_Daniil_BetaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
